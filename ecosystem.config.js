1141e2eac316785616b1f420c99f1b9068e3e949
Stephen Davis <stephenedavis17@gmail.com>
2021-08-25 09:56:05 -0400

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [{
+    name: `${process.env.REPO_NAME}`,
+    script: "docker-compose up",
+    env: {
+      ...process.env
+    }
+  }],
+  deploy : {
+    production : {
+      user: process.env.SED_CLOUD_USER,
+      host: process.env.SED_CLOUD_HOST,
+      ref: 'origin/main',
+      key: 'deploy.key',
+      repo: `https://github.com/${process.env.REPO_OWNER}/${process.env.REPO_NAME}.git`,
+      path: `/home/${process.env.SED_CLOUD_USER}/${process.env.REPO_NAME}`,
+      'post-deploy': "docker-compose build && pm2 reload ecosystem.config.js --env production",
+      env: {
+        ...process.env
+      }
+    }
+  }
+};
