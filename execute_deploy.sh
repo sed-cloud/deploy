89c03f85da1dba68f355e3466f88351813e189c4
Stephen Davis <stephenedavis17@gmail.com>
2021-10-03 16:51:21 -0400

    Update execute_deploy.sh

--- execute_deploy.sh
+++ execute_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npx pm2 describe ${{ inputs.repo-name }}
-if (( $? -eq 1 )) ; then 
+if [[ $? -eq 1 ]] ; then 
   env $(cat .env | grep -v \"#\" | xargs) npx pm2 deploy ${{ github.action_path }}/ecosystem.config.js production setup 
 else
   env $(cat .env | grep -v \"#\" | xargs) npx pm2 deploy ${{ github.action_path }}/ecosystem.config.js production update 
