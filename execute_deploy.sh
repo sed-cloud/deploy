e8a7fd28f7ef7fa63ceabc949c495c47753bbea7
Stephen Davis <stephenedavis17@gmail.com>
2021-10-03 22:27:21 -0400

    Update execute_deploy.sh

--- execute_deploy.sh
+++ execute_deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 npx pm2 describe $1
 if [[ $? -eq 1 ]] ; then 
+  echo $1 does not exist running setup 
   env $(cat .env | grep -v \"#\" | xargs) npx pm2 deploy $2/ecosystem.config.js production setup 
 else
+  echo $1 does exist running update 
   env $(cat .env | grep -v \"#\" | xargs) npx pm2 deploy $2/ecosystem.config.js production update 
 fi
 exit 0 
